85d50ff5cc2422df1a6789ca98b6f933d89ef928
tigran <tigr.kman@gmail.com>
2022-06-26 09:00:41 +0300

    delete branch

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,5 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  target: 'static',
-  generate: {
-    fallback: true
-  },
   head: {
     title: 'atomic-app',
     meta: [
